b0337a09a193b85d53d8292a8b2acf1ce18a33fb
Naveen <naveentruly86@gmail.com>
2021-06-01 00:45:10 +0530

    Added: AnalyseMood Function call

--- main/java/com/moodanalysis/MoodAnalyser.java
+++ main/java/com/moodanalysis/MoodAnalyser.java
@@ -1,4 +1,13 @@
 package com.moodanalysis;
 
 public class MoodAnalyser {
+    public String analyseMood(String message) {
+        if (message.contains("sad")) {
+            String sad = null;
+            return sad;
+        } else {
+            String happy = null;
+            return happy;
+        }
+    }
 }
--- test/java/com/moodanalysis/MoodAnalyserTest.java
+++ test/java/com/moodanalysis/MoodAnalyserTest.java
@@ -1,5 +1,13 @@
 package com.moodanalysis;
 
-public class MoodAnalyserTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MoodAnalyserTest {
+    @Test
+    public void testMoodAnalysis() throws Exception {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a sad message");
+        Assert.assertTrue(mood,true);
+    }
 }
